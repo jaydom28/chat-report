5486e96edec3d5864268faefdbbdef1fd026989a
Justin <jaydom28@gmail.com>
2020-12-12 16:44:36 -0500

    Big refactor

--- helpers.py
+++ helpers.py
@@ -2,24 +2,125 @@
 Helper classes and functions to tidy up the code a little.
 """
 import json
-import os
 
-class Person():
+from collections import defaultdict, Counter
+from threading import Thread
+
+class FileReader:
+    """
+    Base class for reading message data from a file and creating message objects.
+    """
+    def __init__(self, path):
+        self.path = path
+        self.content =""
+        self.messages = []
+
+    def read(self):
+        """
+        Return the contents of self.path
+        """
+        with open(self.path, "r") as message_file:
+            self.content = message_file.read()
+
+    @classmethod
+    def from_strings(cls, file_paths):
+        """
+        Return a list of initialized FileReader objects.
+        """
+        return [cls(f_path) for f_path in file_paths]
+
+
+class FBFileReader(FileReader):
+    """
+    Reads JSON file containing message data from facebook and creates FBMessage
+    objects
+    """
+    def read(self):
+        """
+        Creates message objects from message files downloaded from facebook
+        """
+        super().read()
+        file_data = json.loads(self.content)
+        self.messages = [FBMessage(msg) for msg in file_data["messages"]]
+
+
+class Message:
+    """
+    Base class for holding data from a message.
+    """
+    def __init__(self, sender, text, time_sent=None):
+        self.sender = sender
+        self.text = text
+        self.time_sent = time_sent
+
+    @classmethod
+    def from_readers(cls, file_readers):
+        """
+        Returns a list of initialized message objects when given a list of readers.
+        """
+        threads = [Thread(target=r.read) for r in file_readers]
+        for thread in threads: thread.start()
+        for thread in threads: thread.join()
+        messages = []
+        for reader in file_readers:
+            messages.extend(reader.messages)
+        return messages
+
+
+class FBMessage(Message):
+    """
+    Holds data for a single facebook message.
+    """
+    def __init__(self, msg_dict):
+        sender = msg_dict.get("sender_name", "")
+        text = msg_dict.get("content", "")
+        time_sent = float(msg_dict.get("timestamp_ns", 0))
+        super().__init__(sender=sender, text=text, time_sent=time_sent)
+
+def create_file_readers(file_paths: list) -> list:
+    """
+    Take a list of file paths and return a list of FileReader objects from
+    those paths.
+    :file_paths: - list of strings where each string is a path to a file
+    :return: - list of FileReader objects
+    """
+    return [FBFileReader(f_path) for f_path in file_paths]
+
+
+class Person:
     """
     The Person class contains message data of a particular user as well as
     methods that facilitate getting particular data.
     """
-    def __init__(self, name):
+    def __init__(self, name, messages=None):
         self.name = name
-        self.messages = []
-        self.words = {}
-        self.out_reacts = {} # Reactions this person has given to others
-        self.in_reacts = {} # Reactions this person has received on their messages
+        self.messages = messages if messages is not None else []
+        self._words = None
         self._word_count = None
         self._char_count = None
 
-    def __str__(self):
-        return self.name
+    @classmethod
+    def from_strings(cls, file_paths):
+        """
+        Return a dict of Person objects initialized from files in file_paths.
+        """
+        file_readers = FBFileReader.from_strings(file_paths)
+        messages = Message.from_readers(file_readers)
+        name_msg_dict = defaultdict(list)
+        for msg in messages:
+            name_msg_dict[msg.sender].append(msg)
+        return [cls(name, msgs) for (name, msgs) in name_msg_dict.items()]
+
+    @property
+    def words(self):
+        """
+        A dict that maps words -> word_counts.
+        """
+        if self._words is None:
+            self._words = Counter()
+            for msg in self.messages:
+                self._words += Counter(msg.text.split())
+        return self._words
 
     @property
     def word_count(self):
@@ -27,9 +128,7 @@ class Person():
         An integer representing the total number of words that the user has sent
         """
         if self._word_count is None:
-            if self.words == {}:
-                self._initialize_word_counts()
-            self._word_count = sum(x for x in self.words.values())
+            self._word_count = sum(count for count in self.words.values())
         return self._word_count
 
     @property
@@ -38,107 +137,5 @@ class Person():
         An integer representing the total number of characters that the user has sent
         """
         if self._char_count is None:
-            if self.words == {}:
-                self._initialize_word_counts()
-            self._char_count = sum([len(word) * count for (word, count) in self.words.items()])
+            self._char_count = sum(len(msg.text) for msg in self.messages)
         return self._char_count
-
-    def _initialize_word_counts(self):
-        for msg in self.messages:
-            for word in msg["content"].split():
-                self.words[word] = self.words.get(word, 0) + 1
-    
-    def _initialize_in_reacts(self):
-        for msg in self.messages:
-            if "reactions" in msg:
-                for react in msg["reactions"]:
-                    self.in_reacts[react["reaction"]] = self.in_reacts.get(react["reaction"], 0) + 1
-
-    def get_word_count(self, word: str) -> int:
-        """
-        Get the number of times the user has said a particular word
-
-        :word: - a string of the word to get a count for
-        :return: - an integer, number of times the word has been typed
-        """
-        if self.words == {}:
-            self._initialize_word_counts()
-        return self.words.get(word, 0)
-
-
-
-def read_messages(file_paths: list, verbose: bool = False) -> list:
-    """
-    Go through json files containing facebook messages and load them all into
-    a list of tuples in the form (sender_name, message_content)
-
-    :file_paths: - a list of strings of paths leading to files containing facebook messages
-    :return: - a list of tuples in the form (sender_name, message_content)
-    """
-    if verbose:
-        print("Now reading in the following files:")
-
-    messages = []
-    for path in file_paths:
-        if not os.path.exists(path):
-            print("The following file does not exist:", path)
-            exit()
-        messages.extend(_read_messages_from_file(path))
-        if verbose:
-            print("\t" + path)
-
-    return messages
-
-def _read_messages_from_fb_json(file_path: str):
-    """
-    Reads the messages from a json file generated when you download your
-    facebook data from the facebook website.
-    """
-    with open(file_path, "r") as msg_file:
-        messages = json.load(msg_file)["messages"]
-    return messages
-
-def _read_messages_from_file(file_path: str, file_read_func=_read_messages_from_fb_json) -> list:
-    """
-    Extract messages from a single file.
-
-    :file_path: - string indicating the path of a file containing messages.
-    :return: - a list of tuples in the form (sender_name, message_content)
-    """
-    return file_read_func(file_path)
-
-def get_person_objects_from_messages(messages: list, verbose=False) -> list:
-    """
-    Create a list of Person objects from a list of messages.
-
-    :messages: - a list of dicts representing messages, must contain "content"
-                 and "sender_name" keys
-    :return: - a list of Person objects with their messages initialized
-    """
-    if verbose:
-        print("Now creating person objects from the messages.")
-
-    person_objects = {}
-    for msg in messages:
-        if msg["sender_name"] not in person_objects:
-            person_objects[msg["sender_name"]] = Person(msg["sender_name"])
-        if "content" in msg:
-            msg["content"] = "".join([c.lower() for c in msg["content"] if c.isalnum() or c.isspace()])
-            person_objects[msg["sender_name"]].messages.append(msg)
-        if "reactions" in msg:
-            for reaction in msg["reactions"]:
-                actor = reaction["actor"]
-                action = reaction["reaction"]
-                if actor not in person_objects:
-                    person_objects[actor] = Person(actor)
-                person_objects[actor].out_reacts[action] = \
-                    person_objects[actor].out_reacts.get(action, 0) + 1
-
-    max_name_len = max([len(p.name) for p in person_objects.values()])
-    max_dig_len = max([len(str(len(p.messages))) for p in person_objects.values()])
-
-    if verbose:
-        output_str = "%%%ds : %%%dd messages" % (max_name_len, max_dig_len)
-        for name, p_obj in person_objects.items():
-            print(output_str % (name, len(p_obj.messages)))
-    return list(person_objects.values())
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 from argparse import ArgumentParser
 
-import helpers
+from helpers import Person
 
 def parse_args(args):
     """
@@ -19,26 +19,13 @@ def parse_args(args):
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true")
     return parser.parse_args(args)
 
+
 if __name__ == "__main__":
     ARGS = parse_args(sys.argv[1:])
-    # Read in messages
-    MESSAGES = helpers.read_messages(ARGS.files, ARGS.verbose)
-    print("There are %d messages" % len(MESSAGES))
-    USERS = helpers.get_person_objects_from_messages(MESSAGES, ARGS.verbose)
+    USERS = Person.from_strings(ARGS.files)
     for u in USERS:
-        name = u.name
-        msg_count = len(u.messages)
-        wc = u.word_count
-        cc = u.char_count
-        print("%s %d messages %d words %d characters" % (name, msg_count, wc, cc))
-    print("%d total messages" % sum(len(u.messages) for u in USERS))
-    print("%d total words" % sum(u.word_count for u in USERS))
-    print("%d total characters" % sum(u.char_count for u in USERS))
-    import json
-    with open("sample_data.json", "w") as f:
-        blah = [(p.name, p.words) for p in USERS]
-        blah = dict(blah)
-        f.write(json.dumps(blah, indent=4, sort_keys=True))
+        name, mc, wc, cc = u.name, len(u.messages), u.word_count, u.char_count
+        print(f'{name}: {mc} messages, {wc} words, {cc} characters')
 
     # Analyze Messages
     # Create Charts
